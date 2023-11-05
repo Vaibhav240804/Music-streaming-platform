28fe8d77d04a0e19f2355606cf691da172e80163
Adii2202 <adiningule2@gmail.com>
2023-11-06 00:42:09 +0530

    pages added

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/loginuser")
 def userlogin():
-  return render_template("loginUser.html")
+    return render_template("loginUser.html")
+
+
+@app.route("/loginadmin")
+def adminlogin():
+    return render_template("loginAdmin.html")
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
