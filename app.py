b4ddc4b66d41b33cbc4f253e8a36f3e00b9e854d
Vaibhav <direct2vaibhavkore@gmail.com>
2023-11-06 00:04:46 +0530

    added login

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello World!"
+
+@app.route("/login")
+def userlogin():
+  return return_template("loginUser.html")
 
 if __name__ == "__main__":
   app.run()
